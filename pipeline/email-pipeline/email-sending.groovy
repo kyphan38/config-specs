0d57f2d31f6dceec7d3e331d0f7d9eb254ad58f2
kyphan38 <kyphan.work@gmail.com>
2025-03-17 00:27:19 +0700

    feat: email notification

--- pipeline/email-pipeline/email-sending.groovy
+++ pipeline/email-pipeline/email-sending.groovy
@@ -1,44 +1,44 @@
 #!/usr/bin/env groovy
 
-def expiry15Days = []
-def expiry25Days = []
-def workingDir = "pipeline/email-pipeline"
+String expiry15Days = "[[username:a-db-user-1, expiry:7, env:a], [username:a-db-user-2, expiry:3, env:a], [username:b-db-user-1, expiry:4, env:b], [username:b-db-user-2, expiry:7, env:b], [username:c-db-user-1, expiry:1, env:c], [username:c-db-user-2, expiry:2, env:c"
+String expiry25Days = "[[username:a-db-user-3, expiry:15, env:a], [username:b-db-user-3, expiry:15, env:b], [username:c-db-user-3, expiry:15, env:c]]"
+String workingDir = "pipeline/email-pipeline"
 
 
 pipeline() {
   agent 'any'
 
   stages {
-    stage('Execute') {
-      steps {
-        script {
-          def envs = ["dev", "test", "nonprod", "prod"]
+    // stage('Execute') {
+    //   steps {
+    //     script {
+    //       def envs = ["dev", "test", "nonprod", "prod"]
 
-          for (env in envs) {
+    //       for (env in envs) {
 
-            // example_output is a map
-            def example_output = [:]
+    //         // example_output is a map
+    //         def example_output = [:]
 
-            example_output["user_a"] = 25
-            if (env == "test") {
-              example_output["user_a"] = 15
-            }
-            example_output["user_b"] = 25
-            example_output["user_c"] = 15
+    //         example_output["user_a"] = 25
+    //         if (env == "test") {
+    //           example_output["user_a"] = 15
+    //         }
+    //         example_output["user_b"] = 25
+    //         example_output["user_c"] = 15
 
-            example_output.each { user, expiryDays ->
-              if (expiryDays == 15) {
-                expiry15Days << ["username": user, "expiry": expiryDays, "env": env] // List of Maps
-              } else if (expiryDays == 25) {
-                expiry25Days << ["username": user, "expiry": expiryDays, "env": env]
-              }
-            }
-          }
-          echo "${expiry15Days}"
-          echo "${expiry25Days}"
-        }
-      }
-    }
+    //         example_output.each { user, expiryDays ->
+    //           if (expiryDays == 15) {
+    //             expiry15Days << ["username": user, "expiry": expiryDays, "env": env] // List of Maps
+    //           } else if (expiryDays == 25) {
+    //             expiry25Days << ["username": user, "expiry": expiryDays, "env": env]
+    //           }
+    //         }
+    //       }
+    //       echo "${expiry15Days}"
+    //       echo "${expiry25Days}"
+    //     }
+    //   }
+    // }
 
     stage('Prepare report') {
       steps {
@@ -48,7 +48,7 @@ pipeline() {
             python3 -m venv ${workingDir}/venv
             source ${workingDir}/venv/bin/activate
             pip3 install -r ${workingDir}/requirements.txt
-            python3 ${workingDir}/test.py '${expiry15Days}' '${expiry25Days}'
+            python3 ${workingDir}/main.py '${expiry15Days}' '${expiry25Days}'
           """
         }
       }
@@ -57,10 +57,9 @@ pipeline() {
     stage('Sending email') {
       steps {
         script {
-          
           emailext (
             mimeType: 'text/html',
-            body: '${FILE,path="formatted-report.html"}',
+            body: '${FILE,path="email-report.html"}',
             subject: 'User Expiry Report',
             from: 'tienky30082002@gmail.com',
             to: 'andy30082002@gmail.com'
--- pipeline/email-pipeline/main.py
+++ pipeline/email-pipeline/main.py
@@ -1,90 +1,47 @@
-# def extract_data(s):
-#     # Remove outer brackets
-#     s = s[1:-1]
-    
-#     # Split into individual entries and filter out empty strings
-#     parts = [p for p in s.split("[") if "]" in p]
-#     res = [p.split("]")[0] for p in parts]
-    
-#     # Convert each entry into a dictionary
-#     result = []
-#     for item in res:
-#         # Split by comma and create key-value pairs
-#         pairs = [pair.strip() for pair in item.split(",")]
-#         entry_dict = {}
-#         for pair in pairs:
-#             key, value = pair.split(":")
-#             entry_dict[key] = value
-#         result.append(entry_dict)
-    
-#     return result
-  
-
-
-# if __name__ == "__main__":
-#   expiry_within_7 = "[[username:a-db-user-1, expiry:7, env:a], [username:a-db-user-2, expiry:3, env:a], [username:b-db-user-1, expiry:4, env:b], [username:b-db-user-2, expiry:7, env:b], [username:c-db-user-1, expiry:1, env:c], [username:c-db-user-2, expiry:2, env:c]]"
-#   expiry_within_7_data = extract_data(expiry_within_7)
-#   expiry_within_15 = "[[username:a-db-user-3, expiry:15, env:a], [username:b-db-user-3, expiry:15, env:b], [username:c-db-user-3, expiry:15, env:c]]"
-#   expiry_within_15_data = extract_data(expiry_within_15)
-
-#   print(expiry_within_7_data)
-  
-
-  # expiry_within_15 = "[[username:a-db-user-3, expiry:15, env:a], [username:b-db-user-3, expiry:15, env:b], [username:c-db-user-3, expiry:15, env:c]]"
-  # expiry_within_15_data = extract_data(expiry_within_15)
-
-
 from jinja2 import Environment, FileSystemLoader
 import os
+import sys
 
-def extract_data(s):
-    s = s[1:-1]
-    parts = [p for p in s.split("[") if "]" in p]
-    res = [p.split("]")[0] for p in parts]
-    
-    result = []
-    for item in res:
-        pairs = [pair.strip() for pair in item.split(",")]
-        entry_dict = {}
-        for pair in pairs:
-            key, value = pair.split(":")
-            if key == "expiry":
-                entry_dict[key] = int(value)  # Convert expiry to integer
-            else:
-                entry_dict[key] = value
-        result.append(entry_dict)
-    
-    return result
+def parse_string_to_dicts(string):
+  string = string[1:-1]
+  
+  parts = [part for part in string.split("[") if "]" in part]
+  entries = [entry.split("]")[0] for entry in parts]
+  
+  user_entries = []
+  for entry in entries:
+    key_value_pairs = [pair.strip() for pair in entry.split(",")]
+    user_dict = {}
+    for pair in key_value_pairs:
+      key, value = pair.split(":")
+      user_dict[key] = value
+    user_entries.append(user_dict)
+  
+  return user_entries
 
-def generate_html_report(template_path, output_path, expiry_within_7_data, expiry_within_15_data):
-
-    # Set up Jinja2 environment
-    env = Environment(loader=FileSystemLoader(os.path.dirname(template_path)))
-    template = env.get_template(os.path.basename(template_path))
-    
-    # Render the template with data
-    html_content = template.render(
-        expiry_within_7=expiry_within_7_data,
-        expiry_within_15=expiry_within_15_data
-    )
-    
-    # Write to output file
-    with open(output_path, 'w') as f:
-        f.write(html_content)
-    
-    return html_content
+def generate_html_report(expiry_within_7_users, expiry_within_15_users, template_path, output_path):
+  env = Environment(loader=FileSystemLoader(os.path.dirname(template_path)))
+  template = env.get_template(os.path.basename(template_path))
+  
+  html_content = template.render(
+    expiry_within_7=expiry_within_7_users,
+    expiry_within_15=expiry_within_15_users
+  )
+  
+  with open(output_path, 'w') as f:
+    f.write(html_content)
+  
+  return html_content
 
 if __name__ == "__main__":
-    expiry_within_7 = "[[username:a-db-user-1, expiry:7, env:a], [username:a-db-user-2, expiry:3, env:a], [username:b-db-user-1, expiry:4, env:b], [username:b-db-user-2, expiry:7, env:b], [username:c-db-user-1, expiry:1, env:c], [username:c-db-user-2, expiry:2, env:c]]"
-    expiry_within_15 = "[[username:a-db-user-3, expiry:15, env:a], [username:b-db-user-3, expiry:15, env:b], [username:c-db-user-3, expiry:15, env:c]]"
-    
-    # Extract data
-    expiry_within_7_data = extract_data(expiry_within_7)
-    expiry_within_15_data = extract_data(expiry_within_15)
-    
-    # Generate HTML report
-    template_path = "./templates/email-report.html"
-    output_path = "report.html"
-    html_content = generate_html_report(template_path, output_path, expiry_within_7_data, expiry_within_15_data)
-    
-    print("Report generated successfully at:", output_path)
+  expiry_within_7_string = sys.argv[1]
+  expiry_within_15_string = sys.argv[2]
+
+  expiry_within_7_users = parse_string_to_dicts(expiry_within_7_string)
+  expiry_within_15_users = parse_string_to_dicts(expiry_within_15_string)
+  
+  template_path = "./templates/raw-email-report.html"
+  output_path = "email-report.html"
+  html_content = generate_html_report(expiry_within_7_users, expiry_within_15_users, template_path, output_path)
+  
+  print("Report generated successfully at:", output_path)
