3c87e440adbd2d31fde58875bb48d049b80ebb97
Ky Phan <106050242+kyphan38@users.noreply.github.com>
2025-03-17 00:42:06 +0700

    feat: email notification on jenkins (#2)

---
+++ pipeline/email-pipeline/email-sending.groovy
@@ -0,0 +1,101 @@
+#!/usr/bin/env groovy
+
+String expiry15Days = "[[username:a-db-user-1, expiry:7, env:a], [username:a-db-user-2, expiry:3, env:a], [username:b-db-user-1, expiry:4, env:b], [username:b-db-user-2, expiry:7, env:b], [username:c-db-user-1, expiry:1, env:c], [username:c-db-user-2, expiry:2, env:c"
+String expiry25Days = "[[username:a-db-user-3, expiry:15, env:a], [username:b-db-user-3, expiry:15, env:b], [username:c-db-user-3, expiry:15, env:c]]"
+String workingDir = "pipeline/email-pipeline"
+String templatePath = "templates/raw-email-report.html"
+String templateOutputPath = "email-report.html"
+
+
+pipeline() {
+  agent 'any'
+
+  stages {
+    stage('Prepare report') {
+      steps {
+        script {
+
+          sh """
+            cd ${workingDir}
+            python3 -m venv venv
+            source venv/bin/activate
+            pip3 install -r requirements.txt
+            python3 main.py '${expiry15Days}' '${expiry25Days}' '${templatePath}' '${templateOutputPath}'
+          """
+        }
+      }
+    }
+
+    stage('Sending email') {
+      steps {
+        script {
+          def reportPath = "${workingDir}/${templateOutputPath}"
+
+          emailext (
+            mimeType: 'text/html',
+            body: '${FILE,path="' + reportPath + '"}',
+            subject: 'User Expiry Report',
+            from: 'a@gmail.com',
+            to: 'b@gmail.com'
+          )
+        }
+      }
+    }
+  }
+
+  post {
+    always {
+      cleanWs()
+    }
+  }
+}
+
+    // stage('SSH') {
+    //   steps {
+    //     script {
+    //       def envs = ["a", "b", "c"]
+    //       def servers = [
+    //         "a": "abc.com",
+    //         "b": "bcd.com",
+    //         "c": "cde.com"
+    //       ]
+
+    //       for (env in envs) {
+    //         def res = sh(script: """
+    //           ssh -o StrictHostKeyChecking=no abc@${servers[env]} 'cd ./hcv-pipeline-test; ./getABC.sh ${env}'
+    //         """, returnStdout: true).trim()
+    //       }
+    //     }
+    //   }
+    // }
+
+    // stage('Execute') {
+    //   steps {
+    //     script {
+    //       def envs = ["dev", "test", "nonprod", "prod"]
+
+    //       for (env in envs) {
+
+    //         // example_output is a map
+    //         def example_output = [:]
+
+    //         example_output["user_a"] = 25
+    //         if (env == "test") {
+    //           example_output["user_a"] = 15
+    //         }
+    //         example_output["user_b"] = 25
+    //         example_output["user_c"] = 15
+
+    //         example_output.each { user, expiryDays ->
+    //           if (expiryDays == 15) {
+    //             expiry15Days << ["username": user, "expiry": expiryDays, "env": env] // List of Maps
+    //           } else if (expiryDays == 25) {
+    //             expiry25Days << ["username": user, "expiry": expiryDays, "env": env]
+    //           }
+    //         }
+    //       }
+    //       echo "${expiry15Days}"
+    //       echo "${expiry25Days}"
+    //     }
+    //   }
+    // }
---
+++ pipeline/email-pipeline/main.py
@@ -0,0 +1,47 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+import sys
+
+def parse_string_to_dicts(string):
+  string = string[1:-1]
+  
+  parts = [part for part in string.split("[") if "]" in part]
+  entries = [entry.split("]")[0] for entry in parts]
+  
+  user_entries = []
+  for entry in entries:
+    key_value_pairs = [pair.strip() for pair in entry.split(",")]
+    user_dict = {}
+    for pair in key_value_pairs:
+      key, value = pair.split(":")
+      user_dict[key] = value
+    user_entries.append(user_dict)
+  
+  return user_entries
+
+def generate_html_report(expiry_within_7_users, expiry_within_15_users, template_path, output_path):
+  env = Environment(loader=FileSystemLoader(os.path.dirname(template_path)))
+  template = env.get_template(os.path.basename(template_path))
+  
+  html_content = template.render(
+    expiry_within_7=expiry_within_7_users,
+    expiry_within_15=expiry_within_15_users
+  )
+  
+  with open(output_path, 'w') as f:
+    f.write(html_content)
+  
+  return html_content
+
+if __name__ == "__main__":
+  expiry_within_7_string = sys.argv[1]
+  expiry_within_15_string = sys.argv[2]
+  template_path = sys.argv[3]
+  output_path = sys.argv[4]
+
+  expiry_within_7_users = parse_string_to_dicts(expiry_within_7_string)
+  expiry_within_15_users = parse_string_to_dicts(expiry_within_15_string)
+  
+  html_content = generate_html_report(expiry_within_7_users, expiry_within_15_users, template_path, output_path)
+  
+  print("Report generated successfully at:", output_path)
